4f2e7651fe4fc2b9a39ea6179e5006be362a9c84
Robert Hewitt <fuzzybear@gmail.com>
2022-12-03 18:14:07 +0000

    day3 pt1

--- day03/index.js
+++ day03/index.js
@@ -5,15 +5,23 @@ const lines = async file => (await readFile(file, { encoding: "utf-8" })).split(
 const test = await lines(`./day${day}/testdata.txt`)
 const real = await lines(`./day${day}/puzzle.txt`)
 
-const pt1 = data => data.reduce((pr, [o, _, m])=> {
-    const myI = me.indexOf(m)
-    const opI = op.indexOf(o)
-    return pr + score[myI] + (myI === opI ? 3 : ((myI + 1) % 3) === opI ? 0 : 6)
+const pt1 = data => data.reduce((pr, line)=> {
+    const sub1 = line.substring(0,line.length/2)
+    const sub2 = line.substring(line.length/2)
+    for (let x of sub1) {
+        if (sub2.includes(x)) {
+            const val = x.charCodeAt(0) - (x.toLowerCase() === x ? 96 : 38)
+            console.log({sub1, sub2, x, val})
+            return pr + val
+        }
+    }
+    
+    return pr
 }, 0)
 
 // // pt1
 console.log({ pt1test: pt1(test) })
-// console.log({ pt1real: pt1(real) })
+console.log({ pt1real: pt1(real) })
 
 // // pt2
 // console.log({ pt2test: pt2(test), expects: 12 })
