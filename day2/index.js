b1c37b82761bd3629ddbd5869809888848db703d
Robert Hewitt <fuzzybear@gmail.com>
2022-12-03 07:51:37 +0000

    refactor day1

--- day2/index.js
+++ day2/index.js
@@ -4,17 +4,11 @@ const lines = async file => (await readFile(file, { encoding: "utf-8" })).split(
 const test = await lines('./day2/testdata.txt')
 const real = await lines('./day2/puzzle.txt')
 
-const pt1 = data => data.reduce((pr, [o, _, m])=> {
-    const myI = me.indexOf(m)
-    const opI = op.indexOf(o)
-    return pr + score[myI] + (myI === opI ? 3 : ((myI + 1) % 3) === opI ? 0 : 6)
-}, 0)
+const pt1 = data => data.map(([o, _, m]) => [op.indexOf(o), me.indexOf(m)])
+    .reduce((pr, [oi, mi]) => pr + score[mi] + (mi === oi ? 3 : ((mi + 1) % 3) === oi ? 0 : 6), 0)
 
-const pt2 = data => data.reduce((pr, [o, _, m])=> {
-    const myI = me.indexOf(m)
-    const opI = op.indexOf(o)
-    return pr + (myI === 0 ? (0 + score[opI === 0 ? 2 : opI -1]) : myI === 1 ? (3 + score[opI]) : (6 + score[(opI +1) %3]))
-}, 0)
+const pt2 = data => data.map(([o, _, m]) => [op.indexOf(o), me.indexOf(m)])
+    .reduce((pr, [oi, mi]) => pr + (mi === 0 ? (0 + score[oi === 0 ? 2 : oi - 1]) : mi === 1 ? (3 + score[oi]) : (6 + score[(oi + 1) % 3])), 0)
 
 const op =      ["A","B","C"] // [r,p,s]
 const me =      ["X","Y","Z"] // [r,p,s] | [l,d,w]
