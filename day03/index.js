dee5a5adcebafa6855be13dc07c4f685e32efc79
Robert Hewitt <fuzzybear@gmail.com>
2022-12-03 21:49:30 +0000

    day 3 pt 2 done

--- day03/index.js
+++ day03/index.js
@@ -5,24 +5,14 @@ const lines = async file => (await readFile(file, { encoding: "utf-8" })).split(
 const test = await lines(`./day${day}/testdata.txt`)
 const real = await lines(`./day${day}/puzzle.txt`)
 
-const pt1 = data => data.reduce((pr, line)=> {
-    const sub1 = line.substring(0,line.length/2)
-    const sub2 = line.substring(line.length/2)
-    for (let x of sub1) {
-        if (sub2.includes(x)) {
-            const val = x.charCodeAt(0) - (x.toLowerCase() === x ? 96 : 38)
-            console.log({sub1, sub2, x, val})
-            return pr + val
-        }
-    }
-    
-    return pr
-}, 0)
+const pt1 = data => data.map(x => [x.substring(0, x.length / 2), x.substring(x.length / 2)]).reduce((pr, [a, b]) => { for (let x of a) if (b.includes(x)) return pr + (x.charCodeAt(0) - (x.toLowerCase() === x ? 96 : 38)) }, 0)
+
+const pt2 = ([a, b, c, ...other]) => { for (let x of a) for (let y of b) for (let z of c) if (x === y && x === z) return (x.charCodeAt(0) - (x.toLowerCase() === x ? 96 : 38)) + (other.length === 0 ? 0 : pt2(other)) }
 
 // // pt1
-console.log({ pt1test: pt1(test) })
-console.log({ pt1real: pt1(real) })
+console.log({ pt1: pt1(test), expect: 157 })
+console.log({ pt1: pt1(real), expect: "?" })
 
 // // pt2
-// console.log({ pt2test: pt2(test), expects: 12 })
-// console.log({ pt2test: pt2(real), expects: "?" })
+console.log({ pt2: pt2(test), expects: 70 })
+console.log({ pt2: pt2(real), expects: "?" })
