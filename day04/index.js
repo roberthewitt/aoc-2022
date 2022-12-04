75c73203527cd1bb168675a3a66273041943a3c3
Robert Hewitt <fuzzybear@gmail.com>
2022-12-04 21:11:51 +0000

    day4 done

--- day04/index.js
+++ day04/index.js
@@ -5,8 +5,15 @@ const lines = async file => (await readFile(file, { encoding: "utf-8" })).split(
 const test = await lines(`./day${day}/testdata.txt`)
 const real = await lines(`./day${day}/puzzle.txt`)
 
-const pt1 = lines => lines.reduce((pr, line) => { const [[a1, a2], [b1, b2]] = line.split(",").map(x => x.split("-").map(Number)); return ((a1 >= b1 && a2 <= b2) || (b1 >= a1 && b2 <= a2)) ? pr + 1 : pr }, 0)
-const pt2 = lines => lines.reduce((pr, line) => { const [[a1, a2], [b1, b2]] = line.split(",").map(x => x.split("-").map(Number)); return ((a1 >= b1 && a1 <= b2) || (a2 >= b1 && a2 <= b2)) || ((b1 >= a1 && b1 <= a2) || (b2 >= a1 && b2 <= a2)) ? pr + 1 : pr }, 0)
+const pt1 = lines => lines.reduce((pr, line) => { 
+    const [[a1, a2], [b1, b2]] = line.split(",").map(x => x.split("-").map(Number))
+    return ((a1 >= b1 && a2 <= b2) || (b1 >= a1 && b2 <= a2)) ? pr + 1 : pr 
+}, 0)
+
+const pt2 = lines => lines.reduce((pr, line) => { 
+    const [[a1, a2], [b1, b2]] = line.split(",").map(x => x.split("-").map(Number))
+    return ((a1 >= b1 && a1 <= b2) || (a2 >= b1 && a2 <= b2)) || ((b1 >= a1 && b1 <= a2) || (b2 >= a1 && b2 <= a2)) ? pr + 1 : pr 
+}, 0)
 
 // // pt1
 console.log({ pt1: pt1(test), expect: 2 })
