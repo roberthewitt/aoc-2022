728368a464e77b2c9286e5f64f8410dc1507d976
Robert Hewitt <fuzzybear@gmail.com>
2022-12-04 20:59:45 +0000

    single line answer

--- day04/index.js
+++ day04/index.js
@@ -5,34 +5,8 @@ const lines = async file => (await readFile(file, { encoding: "utf-8" })).split(
 const test = await lines(`./day${day}/testdata.txt`)
 const real = await lines(`./day${day}/puzzle.txt`)
 
-const pt1 = lines => lines.reduce((pr, line, row) => {
-    const [a,b] = line.split(",")
-    const [a1,a2] = a.split("-").map(Number)
-    const [b1,b2] = b.split("-").map(Number)
-    if (a1 >= b1 && a2 <= b2) {
-        // console.log({a1, a2, b1, b2, row: row+1, type:1})
-        return pr +1
-    } else if (b1 >= a1 && b2 <= a2) {
-        // console.log({a1, a2, b1, b2, row: row +1, type:2})
-        return pr +1
-    }
-    return pr
-}, 0)
-
-const pt2 = lines => lines.reduce((pr, line, row) => {
-    const [a,b] = line.split(",")
-    const [a1,a2] = a.split("-").map(Number)
-    const [b1,b2] = b.split("-").map(Number)
-    if ((a1 >= b1 && a1 <= b2) || (a2 >= b1 && a2 <= b2)) {
-        // console.log({a1, a2, b1, b2, row: row+1, type:1})
-        return pr +1
-    } else if ((b1 >= a1 && b1 <= a2) || (b2 >= a1 && b2 <= a2)) {
-        // console.log({a1, a2, b1, b2, row: row +1, type:2})
-        return pr +1
-    }
-    return pr
-}, 0)
-
+const pt1 = lines => lines.reduce((pr, line) => { const [[a1, a2], [b1, b2]] = line.split(",").map(x => x.split("-").map(Number)); return ((a1 >= b1 && a2 <= b2) || (b1 >= a1 && b2 <= a2)) ? pr + 1 : pr }, 0)
+const pt2 = lines => lines.reduce((pr, line) => { const [[a1, a2], [b1, b2]] = line.split(",").map(x => x.split("-").map(Number)); return ((a1 >= b1 && a1 <= b2) || (a2 >= b1 && a2 <= b2)) || ((b1 >= a1 && b1 <= a2) || (b2 >= a1 && b2 <= a2)) ? pr + 1 : pr }, 0)
 
 // // pt1
 console.log({ pt1: pt1(test), expect: 2 })
@@ -40,4 +14,4 @@ console.log({ pt1: pt1(real), expect: "540", tooLow: [169], tooHigh: [567] })
 
 // // pt2
 console.log({ pt2: pt2(test), expects: 4 })
-console.log({ pt2: pt2(real), expects: "?" })
+console.log({ pt2: pt2(real), expects: "872" })
